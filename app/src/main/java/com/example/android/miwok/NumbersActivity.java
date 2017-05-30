42f0b5caaaa286f0a7c1b4d11cf06c37ffaaad34
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-05-30 15:56:04 -0400

    initial commit

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -24,5 +24,10 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
     }
+
+
+
 }
