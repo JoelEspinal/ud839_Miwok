a5cc975d246c7a77a2fc61df046d052afd5b33e5
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-05-30 22:43:21 -0400

    add ArrayList with simple_list_item_1

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -41,8 +45,10 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
 
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
-
-
-
 }
