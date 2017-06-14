63f2aa39502fbfcaf546e0fbeccf2a9da9d5023b
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-06-13 21:01:14 -0400

    create Word class with to attributes: {defaultTranslation, miwokTranslation}

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,24 @@
+package com.example.android.miwok;
+
+/**
+ * Created by User on 6/13/2017.
+ */
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
