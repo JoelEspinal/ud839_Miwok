35b4ee3ab28629b44565d520333eae2e9ca212ce
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-06-14 15:00:51 -0400

    use new word constructor, set image resource

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -32,16 +32,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Create an ArrayList of words
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo’e"));
-        words.add(new Word("ten", "na’aacha"));
+        words.add(new Word("one", "lutti", R.mipmap.number_one));
+        words.add(new Word("two", "otiiko", R.mipmap.number_two));
+        words.add(new Word("three", "tolookosu", R.mipmap.number_three));
+        words.add(new Word("four", "oyyisa", R.mipmap.number_four));
+        words.add(new Word("five", "massokka", R.mipmap.number_five));
+        words.add(new Word("six", "temmokka", R.mipmap.number_six));
+        words.add(new Word("seven", "kenekaku", R.mipmap.number_seven));
+        words.add(new Word("eight", "kawinta", R.mipmap.number_eight));
+        words.add(new Word("nine", "wo’e", R.mipmap.number_nine));
+        words.add(new Word("ten", "na’aacha", R.mipmap.number_ten));
 
         WordAdapter adapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
