8137b89078fdaa637aa1ec15d4b201ded2ac7aff
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-07-09 00:02:55 -0400

    moving to fragments

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -28,104 +28,13 @@ import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            // Now that the sound file has finished playing, release the media player resources.
-            releaseMediaPlayer();
-        }
-    };
-
-    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener(){
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
-                mMediaPlayer.start();
-            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
-                // Stop playback and clean up resources
-                releaseMediaPlayer();
-            }
-        }
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
 
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("father", "әpә", R.mipmap.family_father, R.raw.family_father));
-        words.add(new Word("mother", "әṭa", R.mipmap.family_mother, R.raw.family_mother));
-        words.add(new Word("son", "angsi", R.mipmap.family_son, R.raw.family_son));
-        words.add(new Word("daughter", "tune", R.mipmap.family_daughter, R.raw.family_daughter));
-        words.add(new Word("older brother", "taachi", R.mipmap.family_older_brother,
-                R.raw.family_older_brother));
-        words.add(new Word("younger brother", "chalitti", R.mipmap.family_younger_brother,
-                R.raw.family_younger_brother));
-        words.add(new Word("older sister", "teṭe", R.mipmap.family_older_sister,
-                R.raw.family_older_sister));
-        words.add(new Word("younger sister", "kolliti", R.mipmap.family_younger_sister,
-                R.raw.family_younger_sister));
-        words.add(new Word("grandmother ", "ama", R.mipmap.family_grandmother,
-                R.raw.family_grandmother));
-        words.add(new Word("grandfather", "paapa", R.mipmap.family_grandfather,
-                R.raw.family_grandfather));
-
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Word word = words.get(i);
-
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
-                    mMediaPlayer.start();
-
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
     }
 }
